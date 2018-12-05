6498662b95f2096ce0d6ce5fe2283977272d1c15
cnelson27 <cnelson27@outlook.com>
2018-12-05 16:06:42 -0700

    Created Potential Customers Landing Page

---
+++ NorthwestLabs/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwestLabs.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PharmacologyCatalog()
+        {
+            return View();
+        }
+
+        public ActionResult ProtocolCatalog()
+        {
+            return View();
+        }
+
+        public ActionResult GetQuote()
+        {
+            return View();
+        }
+    }
+}
