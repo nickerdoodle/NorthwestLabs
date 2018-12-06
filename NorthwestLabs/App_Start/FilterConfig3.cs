53fb49797a532bae7d28320f7a3c49780e56bbf4
sjbankhead <samuelbankhead@gmail.com>
2018-12-06 08:29:00 -0700

    My solution

---
+++ NorthwestLabs/App_Start/FilterConfig3.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwestLabs
+{
+    public class FilterConfig3
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NorthwestLabs/Controllers/SeattleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwestLabs.Controllers
+{
+    public class SeattleController : Controller
+    {
+        // GET: Seattle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            return View();
+        }
+    }
+}
