453cc8b50bdb1bffe29c1787270116c6e8727d16
nmmahe <nicholasmahe@gmail.com>
2018-12-07 01:12:46 -0700

    put stats

---
+++ NorthwestLabs/Models/UniversalLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    public class UniversalLogin
+    {
+
+        public String Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
